182798ed31d9eb32a7498ed641fd323e96e48a99
MayureshKasabeMMCOE <mayureshkasabe2025.comp@mmcoe.edu.in>
2025-09-30 16:17:54 +0530

    Update Exp-1.c

--- Exp-1.c
+++ Exp-1.c
@@ -5,14 +5,15 @@ int main() {
     printf("Consider any one of the lights to be on, example - (1, 0, 0): \n");
     scanf("%d%d%d", &red, &yellow, &green);
 
-    if (red == 1) {
+    if (red == 1 && green == 0 && yellow == 0) {
         printf("Stop\n");
-    } else if (green == 1) {
+    } else if (green == 1 && red == 0 && yellow == 0) {
         printf("Go\n");
-    } else if (yellow == 1) {
+    } else if (yellow == 1 && red == 0 && green == 0) {
         printf("Prepare to Stop\n");
+    } else {
+        printf("Invalid inputs");
     }
 
     return 0;
 }
-
